c2564f4cc5494c4619b769fdd5b8320b8b0a5d56
David <davidt.mp3@gmail.com>
2021-04-29 23:41:49 -0400

    Color Selection and Pretty Materials!

--- SorryReplay/Assets/SorryAssets/Scripts/Pawn.cs
+++ SorryReplay/Assets/SorryAssets/Scripts/Pawn.cs
@@ -7,21 +7,17 @@ using UnityEngine.UI;
 
 public class Pawn : MonoBehaviour
 {
-    public AudioSource audioSource;
+
+   
     // Start is called before the first frame update
     void Start()
     {
-
+        gameObject.GetComponent<Renderer>().material.color = PlayerColor.playerColor;
     }
 
     // Update is called once per frame
     void Update()
     {
        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        ///audioSource.Play();
     }
 }
---
+++ SorryReplay/Assets/SorryAssets/Scripts/PlayerColor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerColor : MonoBehaviour
+{
+    public Color yellow = new Color(1f, 0.75f, 0.04f);
+    public Color green = new Color(0.17f, 0.58f, 0.28f);
+    public Color blue = new Color(0.01f, 0.39f, 0.49f);
+    public Color red = new Color(0.55f, 0.03f, 0f);
+
+    [SerializeField]
+    public static Color playerColor;
+    public GameObject homeScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void setRed()
+    {
+        playerColor = red;
+        homeScreen.SetActive(false);
+    }
+
+    public void setYellow()
+    {
+        playerColor = yellow;
+        homeScreen.SetActive(false);
+    }
+
+    public void setGreen()
+    {
+        playerColor = green;
+        homeScreen.SetActive(false);
+    }
+
+    public void setBlue()
+    {
+        playerColor = blue;
+        homeScreen.SetActive(false);
+    }
+}
